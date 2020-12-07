337251e78bc41cd60877b043b3aac2b541fdc412
Pengelite-00 <69671922+Pengelite-00@users.noreply.github.com>
2020-12-07 21:01:01 +0200

    Add files via upload

---
+++ Obstacle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike_POE
+{
+    class Obstacle : Tile
+    {
+        public Obstacle (int x, int y) : base(x, y, 'X')
+        {
+
+        }
+
+    }
+    
+}
---
+++ RangedWeapon.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike_POE
+{
+    class RangedWeapon : Weapon
+    {
+
+        public RangedWeapon(WeaponType type, string v, int x, int y) : base(x, y)
+        {
+
+            if (type == WeaponType.Rifle)
+            {
+                base.Damage = 4;
+                base.range = 2;
+                base.Durability = 4;
+                base.Cost = 6;
+            }
+
+            if (type == WeaponType.Longbow)
+            {
+                base.Damage = 5;
+                base.range = 3;
+                base.Durability = 3;
+                base.Cost = 7;
+            }
+        }
+
+        public RangedWeapon(WeaponType Rifle, string z)
+        {
+            this.Rifle = Rifle;
+            this.z = z;
+        }
+
+        public WeaponType weaponType;
+        private WeaponType Rifle;
+        private string z;
+
+        public override string ToString()
+        {
+            return "Ranged weapon at " + X + Y + "Deals: " + Damage;
+        }
+
+    }
+
+        
+    }
+
